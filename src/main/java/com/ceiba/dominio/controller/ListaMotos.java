23c99cb9d33c63d438712242ce448937c8e53fe2
Daniel Castro Jara - Ceiba Software <daniel.castro@ceiba.com.co>
2019-11-19 07:19:06 -0500

    Agregando JpaRepository para probrar mapeo

--- src/main/java/com/ceiba/dominio/controller/ListaMotos.java
+++ src/main/java/com/ceiba/dominio/controller/ListaMotos.java
@@ -1,6 +1,5 @@
 package com.ceiba.dominio.controller;
 
-import java.util.Collection;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,8 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ceiba.infraestructura.model.Moto;
 import com.ceiba.dominio.servicio.IMotoServie;
+import com.ceiba.infraestructura.model.Moto;
 
 
 @RestController
--- src/main/java/com/ceiba/dominio/dao/MotoDao.java
+++ src/main/java/com/ceiba/dominio/dao/MotoDao.java
@@ -1,9 +1,10 @@
 package com.ceiba.dominio.dao;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import com.ceiba.infraestructura.model.Moto;
 
-public interface MotoDao extends CrudRepository<Moto, String>{
+public interface MotoDao extends JpaRepository<Moto, String>{
 
 }
